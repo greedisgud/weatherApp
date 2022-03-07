c44538429d62cb5302916d6754142f16e9e4755e
Evan Hayes <hayesevan7@gmail.com>
2022-03-07 13:26:22 -0500

    finished app

---
+++ app.js
@@ -0,0 +1,34 @@
+const searchForm = document.getElementById("searchForm");
+const searchBox = document.getElementById("searchBox");
+
+let date = document.querySelector(".date");
+date.innerText = new Date().toJSON().slice(0, 10).replace(/-/g, ".");
+
+searchForm.addEventListener("submit", setQuery);
+
+function setQuery(e) {
+  e.preventDefault();
+  getResults(searchBox.value);
+}
+
+function getResults(query) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=8070f39242c0e4c7ac7a1cb709976198`
+  )
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
+  let city = document.querySelector(".city");
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+  let temperature = document.querySelector(".temp");
+  temperature.innerText = `${weather.main.temp} °C`;
+  let currentWeather = document.querySelector(".weather");
+  currentWeather.innerText = `${weather.weather[0].main}`;
+  let highLow = document.querySelector(".highLow");
+  highLow.innerHTML = `${weather.main.temp_max} / ${weather.main.temp_min}<span> °C</span>`;
+}
